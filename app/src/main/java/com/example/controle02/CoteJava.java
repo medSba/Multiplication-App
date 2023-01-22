1a9fddb82bd22a4e62f530bb303d6e5eb40d2410
Ahmed Essebaiy <ahmadessebaiy@gmail.com>
2023-01-22 12:54:12 +0100

    btn reinitialiser and quitter created on java

--- app/src/main/java/com/example/controle02/CoteJava.java
+++ app/src/main/java/com/example/controle02/CoteJava.java
@@ -3,14 +3,36 @@ package com.example.controle02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class CoteJava extends AppCompatActivity {
-
+    Button btn_reinitializer,btn_quitter;
+    EditText Number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cote_xml);
+        btn_reinitializer = findViewById(R.id.btn_rn);
+        btn_quitter = findViewById(R.id.btn_qtt);
+        Number = findViewById(R.id.number);
+
+        btn_reinitializer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Number.setText(null);
+            }
+        });
+
+        btn_quitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+            }
+        });
     }
 }
