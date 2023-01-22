12bb051834b95eb462305c96f48f3d14977b082d
Ahmed Essebaiy <ahmadessebaiy@gmail.com>
2023-01-22 11:13:30 +0100

    Project created

---
+++ app/src/main/java/com/example/controle02/CoteJava.java
@@ -0,0 +1,14 @@
+package com.example.controle02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CoteJava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cote_xml);
+    }
+}
